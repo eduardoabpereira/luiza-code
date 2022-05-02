d1728479c80104c801ad96baa29636bd91a3d0db
Eduardo Pereira <edu.eabp@gmail.com>
2022-05-02 10:45:34 -0300

    fix - htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   target: 'web',
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
